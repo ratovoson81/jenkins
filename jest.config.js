9c9b1ff6c3ddf9fe0c59ce582a3ebe5289aa0d1b
ratovoson81 <Ratovoson81@gmail.com>
2021-08-03 00:46:32 +0300

    update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: [
-    "C:/WINDOWS/system32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/projet_master/src/**/*.test.js",
+    "C:/WINDOWS/system32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/projet_master/src/App.test.js",
     "C:/WINDOWS/system32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/projet_master/src/**/*.test.jsx",
   ],
 };
