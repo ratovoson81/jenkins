8829ac5597a995ee863cd1cdf534488c50bf72b0
ratovoson81 <Ratovoson81@gmail.com>
2021-08-04 22:53:58 +0300

    report coverage file app

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App"
+const { render } = require("@testing-library/react");
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Jenkins");
+  expect(linkElement).toBeInTheDocument();
+});
