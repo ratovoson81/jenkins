847b3b63b95bd3dab1fe8ea9e49669917d19ff43
ratovoson81 <ratovoson81@gmail.com>
2021-08-06 12:20:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>Jenk</p>
+        <p>Jenki</p>
       </header>
     </div>
   );
