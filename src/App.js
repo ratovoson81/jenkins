1cb3d2a662ea33b45daa126b603e3fbc01c8d100
ratovoson81 <Ratovoson81@gmail.com>
2021-08-06 20:22:47 +0300

    error 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>Jenkinqsds</p>
+        <p>Jenkinsd</p>
       </header>
     </div>
   );
