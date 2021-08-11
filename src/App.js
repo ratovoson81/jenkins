c252d3ec8e6cfa05e84c9dff972643636065318e
ratovoson81 <ratovoson81@gmail.com>
2021-08-11 08:56:05 +0300

    refact

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} clasNamef="App-logo" alt="logo" />*/}
-        <p>Jenkns</p>
+        <p>Jenkins</p>
       </header>
     </div>
   );
