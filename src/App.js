b9d63b21929aa82e96ae724d6025c03a2bf9bd9c
ratovoson81 <ratovoson81@gmail.com>
2021-08-11 10:32:32 +0300

    d

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<img src={logo} clasName="App-logo" alt="logo" />*/}
         <p>Jenkins</p>
       </header>
     </div>
