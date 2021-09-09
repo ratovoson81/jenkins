ac18b498726c09af8b0dc32ee53480da6f6304f6
ratovoson81 <Ratovoson81@gmail.com>
2021-09-09 14:18:34 +0300

    demo error

--- src/router/Nav.jsx
+++ src/router/Nav.jsx
@@ -38,12 +38,12 @@ export default function Nav() {
                   >
                     Todos
                   </NavLink>
-                  {/*<NavLink
+                  <NavLink
                     to="/Albums"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Albums
-                  </NavLink>*/}
+                  </NavLink>
                 </div>
               </div>
             </div>
@@ -125,12 +125,12 @@ export default function Nav() {
                 >
                   Todos
                 </NavLink>
-                {/*<NavLink
+                <NavLink
                   to="/Albums"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Album
-                </NavLink>*/}
+                </NavLink>
                 <NavLink
                   exact
                   to="/"
