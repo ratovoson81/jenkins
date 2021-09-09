538cf9c79f4cd35b88afef50ac0308abec272199
ratovoson81 <Ratovoson81@gmail.com>
2021-09-09 09:45:48 +0300

    delete nav mobile

--- src/router/Nav.jsx
+++ src/router/Nav.jsx
@@ -125,12 +125,12 @@ export default function Nav() {
                 >
                   Todos
                 </NavLink>
-                <NavLink
+                {/*<NavLink
                   to="/Albums"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Album
-                </NavLink>
+                </NavLink>*/}
                 <NavLink
                   exact
                   to="/"
